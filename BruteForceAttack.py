f1c39e4b1efc723dd64fdb4b1a9df504463780bd
Todd G <65108263+tgrigo@users.noreply.github.com>
2020-06-15 16:28:05 +0800

    Add files via upload

---
+++ BruteForceAttack.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+# Import the itertools.product which is similae to a for-loop in a generation of expression
+from itertools import product
+
+# Import the characters to be serched for
+from string import ascii_letters, digits, punctuation
+
+# Nominate the password to crack
+# This can be changed to a larger charater set or SHA256 for example but for demo purposes 
+# I have set it to 3 charaters long. If this is changed the range also need to be changed
+passwordSimple = "a$2"
+
+# Then create the character ist to be used for brute force
+string = ascii_letters + digits + punctuation
+
+# Set the lenght of password to brute force, in this case 3
+for l in range(1,3):
+
+# Repeat search scrollingthrough one letter or digit at a time
+        for attempt in product(string, repeat=l + 1):
+
+# Display the scrolling search
+            print(f"Trying password {attempt}")
+
+# provide output when the match is found
+            if"".join(attempt) == passwordSimple:
+                print(f"Password has been cracked! It was :", attempt)
+
+# Break the search when the password has been cracked
+                break
